3f0323b742ca214bdf6e80e54cbfb527f4f78a4d
Nelson Reina (@nelreina) <nelreina@gmail.com>
2019-09-18 12:07:05 -0400

    basic layout and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Navigation from "./components/layout/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <main>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" exact component={Profile} />
+      </main>
     </div>
   );
 }
